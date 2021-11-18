5d6645721bd807ea603d032bf8e5910c6d417351
Ajay pandit <42382145+ajayoneness@users.noreply.github.com>
2021-11-18 20:47:42 +0530

    Update scrap.py

--- codeaj/scrap.py
+++ codeaj/scrap.py
@@ -6,71 +6,71 @@ import requests
 from bs4 import BeautifulSoup
 from google_trans_new import google_translator
 
-
-def googleTranslate(text, language):
-    translator = google_translator()
-    translate_text = translator.translate(text, lang_tgt=language)
-    print(translate_text)
-
-
-def scrapall_translate(url, tag, n, language):
-    req = requests.get(url)
-    soup = BeautifulSoup(req.text, 'html.parser')
-    data = soup.find_all(tag)
-    for i in range(0, n):
-        #try:
-        #return data[i].text.strip()
-        return googleTranslate(str(data[i].text.strip()),language)
-        #except:
-         #   break
+class scrap():
+    def googleTranslate(self,text, language):
+        translator = google_translator()
+        translate_text = translator.translate(text, lang_tgt=language)
+        print(translate_text)
 
 
-def scrapallparagraph(url, tag, n):
-    req = requests.get(url)
-    soup = BeautifulSoup(req.text, 'html.parser')
-    data = soup.find_all(tag)
-    par = ""
-    for i in range(0, n):
-        try:
-            # print (data[i].text.strip())
-            par = par + str(data[i].text.strip())
-
-            # print(par)
-        except:
-            break
-    # print(type(par))
-    return par
+    def scrapall_translate(self,url, tag, n, language):
+        req = requests.get(url)
+        soup = BeautifulSoup(req.text, 'html.parser')
+        data = soup.find_all(tag)
+        for i in range(0, n):
+            #try:
+            #return data[i].text.strip()
+            return self.googleTranslate(str(data[i].text.strip()),language)
+            #except:
+             #   break
 
 
-#  print('after translation')
-#   for i in range(0,499):
-# print(googleTranslate(par[i],language))
+    def scrapallparagraph(self,url, tag, n):
+        req = requests.get(url)
+        soup = BeautifulSoup(req.text, 'html.parser')
+        data = soup.find_all(tag)
+        par = ""
+        for i in range(0, n):
+            try:
+                # print (data[i].text.strip())
+                par = par + str(data[i].text.strip())
+
+                # print(par)
+            except:
+                break
+        # print(type(par))
+        return par
 
 
-def scrapalldata(url, tag):
-    req = requests.get(url)
-    soup = BeautifulSoup(req.text, 'html.parser')
-    data = soup.find_all(tag)
-    for i in range(0, 1000):
-        try:
-            print(f"{i}. {data[i].text.strip()}")
-        except:
-            break
+    #  print('after translation')
+    #   for i in range(0,499):
+    # print(googleTranslate(par[i],language))
+
+
+    def scrapalldata(self,url, tag):
+        req = requests.get(url)
+        soup = BeautifulSoup(req.text, 'html.parser')
+        data = soup.find_all(tag)
+        for i in range(0, 1000):
+            try:
+                print(f"{i}. {data[i].text.strip()}")
+            except:
+                break
+            # googleTranslate(str(data[i].text.strip()),language)
+
+
+    def scrapone(self,url, htmltag):
+        req = requests.get(url)
+        soup = BeautifulSoup(req.text, 'html.parser')
+        data = soup.find_all(htmltag)
+        print(data[0].text.strip())
+
+
+    def scrap_div_class(self,url, clas):
+        req = requests.get(url)
+        bsobj = BeautifulSoup(req.text, 'html.parser')
+        data = bsobj.find_all('div', class_=clas)
+        print(data[0].text.strip())
         # googleTranslate(str(data[i].text.strip()),language)
-
-
-def scrapone(url, htmltag):
-    req = requests.get(url)
-    soup = BeautifulSoup(req.text, 'html.parser')
-    data = soup.find_all(htmltag)
-    print(data[0].text.strip())
-
-
-def scrap_div_class(url, clas):
-    req = requests.get(url)
-    bsobj = BeautifulSoup(req.text, 'html.parser')
-    data = bsobj.find_all('div', class_=clas)
-    print(data[0].text.strip())
-    # googleTranslate(str(data[i].text.strip()),language)
 
 
